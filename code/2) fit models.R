e322b41918ebf29b120720cd9b6f13f34cd8ec62
jswesner <jeff.wesner@usd.edu>
2023-11-10 14:05:41 -0600

    model fits

---
+++ code/2) fit models.R
@@ -0,0 +1,48 @@
+library(brms)
+library(isdbayes)
+
+
+
+# Instead of re-fitting, you can load the fitted models below
+
+# load models (these are already fit)
+fit_temp = readRDS("models/fit_temp.rds")
+fit_om = readRDS("models/fit_om.rds")
+fit_gpp = readRDS("models/fit_gpp.rds")
+fit_temp_om = readRDS("models/fit_temp_om.rds")
+fit_temp_gpp = readRDS("models/fit_temp_gpp.rds")
+fit_om_gpp = readRDS("models/fit_om_gpp.rds")
+fit_temp_om_gpp = readRDS("models/fit_temp_om_gpp.rds")
+
+
+
+
+# Re-fit models -----------------------------------------------------------
+# !!!! Each of the models below takes up to 24 hours to run. They were run on a cluster. Here, we've added them
+# for clarity, but with only a small number of iterations to prevent overwhelming memory. If you fit the models below
+# they should not be used for inference (without increasing the iterations)
+
+#1) load data (NEON body size data)
+dat_all = readRDS("data/derived_data/dat_all.rds")
+
+#2) fit main model
+fit_temp_om_gpp = brm(dw | vreal(no_m2, xmin, xmax) ~ log_om_s*mat_s*log_gpp_s + (1 | sample_id) + (1 | year) + (1 | site_id),
+                      data = dat_all,
+                      stanvars = stanvars,    # required for truncated Pareto via isdbayes package
+                      family = paretocounts(),# required for truncated Pareto via isdbayes package
+                      prior = c(prior(normal(-1.5, 0.2), class = "Intercept"),
+                                prior(normal(0, 0.1), class = "b"),
+                                prior(exponential(7), class = "sd")),
+                      iter = 10, 
+                      chains = 1)
+
+#2) fit submodels using the update function
+
+fit_temp = update(fit_temp_om_gpp, formula = . ~ mat_s + (1 | sample_id) + (1 | year) + (1 | site_id))
+fit_om = update(fit_temp_om_gpp, formula = . ~ log_om_s + (1 | sample_id) + (1 | year) + (1 | site_id))
+fit_gpp = update(fit_temp_om_gpp, formula = . ~ log_gpp_s + (1 | sample_id) + (1 | year) + (1 | site_id))
+fit_temp_om = update(fit_temp_om_gpp, formula = . ~ mat_s*log_om_s + (1 | sample_id) + (1 | year) + (1 | site_id))
+fit_temp_gpp = update(fit_temp_om_gpp, formula = . ~ mat_s*log_gpp_s + (1 | sample_id) + (1 | year) + (1 | site_id))
+fit_om_gpp = update(fit_temp_om_gpp, formula = . ~ log_om_s*log_gpp_s + (1 | sample_id) + (1 | year) + (1 | site_id))
+
+
