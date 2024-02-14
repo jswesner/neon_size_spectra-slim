de021073bc54d2885521f21fe809493b0fcec5d9
jswesner <jeff.wesner@usd.edu>
2024-02-14 16:12:01 -0600

    Add model checking

---
+++ code/3) check models_pp_check_r2.R
@@ -0,0 +1,159 @@
+library(rstan)
+library(tidyverse)
+library(janitor)
+library(tidybayes)
+library(brms)
+library(ggthemes)
+library(isdbayes)
+theme_set(brms::theme_default())
+
+# For these data and models, pp_check() and other functions in brms will not work for model checking. This code does the same thing as pp_check, but it first 
+# accounts for the no_m2 for each body size. This is necessary, because the raw size data "dw" will not match the posterior predictions without first correcting 
+# for no_m2. Because the range of no_m2 is so large, we have to resample dw with replacement, and then plot that resampled data against model predictions.
+
+# 1) load models
+# load models 
+fit_temp = readRDS("models/fit_temp.rds")
+fit_om = readRDS("models/fit_om.rds")
+fit_gpp = readRDS("models/fit_gpp.rds")
+fit_temp_om = readRDS("models/fit_temp_om.rds")
+fit_temp_gpp = readRDS("models/fit_temp_gpp.rds")
+fit_om_gpp = readRDS("models/fit_om_gpp.rds")
+fit_temp_om_gpp = readRDS("models/fit_temp_om_gpp.rds")
+
+# 2) get data
+dat = as_tibble(fit_temp_om_gpp$data)
+
+# 3) extract posteriors 
+posts_sample_lambdas = dat %>% 
+  distinct(sample_id, .keep_all = T) %>% 
+  select(-dw) %>% 
+  mutate(no_m2 = 1) %>% 
+  add_epred_draws(fit_temp_om_gpp, re_formula = NULL) %>% 
+  rename(lambda = .epred) %>% 
+  ungroup()
+
+saveRDS(posts_sample_lambdas, file = "posteriors/posts_sample_lambdas.rds")
+
+# 4) merge posts and raw data
+n_samples = 5000
+
+dat_resampled = dat %>% 
+  group_by(sample_id, log_om_s, log_gpp_s, mat_s, site_id, year) %>% 
+  sample_n(n_samples, weight = no_m2, replace = T) %>% 
+  select(site_id, year, sample_id, no_m2, dw) %>% 
+  group_by(sample_id) %>% 
+  mutate(xmin = min(dw),
+         xmax = max(dw),
+         data = "y_raw") %>% 
+  ungroup()
+
+posts_raw = posts_sample_lambdas %>% 
+  filter(.draw <= 10) %>% 
+  select(lambda, sample_id, .draw) %>% 
+  right_join(dat_resampled %>% 
+               group_by(sample_id) %>% 
+               slice_sample(n = 500) %>% 
+               select(sample_id, dw, no_m2, xmin, xmax, site_id), multiple = "all",
+             relationship = "many-to-many") 
+
+# 5) sample posterior preds
+sim_posts = posts_raw %>% 
+  mutate(x = rparetocounts(lambda = lambda, xmin = xmin, xmax = xmax, n = nrow(.))) %>% 
+  mutate(data = "y_rep") %>% 
+  # mutate(x = round(x,2)) %>% 
+  bind_rows(dat_resampled %>% 
+              mutate(data = "y") %>% 
+              mutate(.draw = 0,
+                     x = dw)) %>% 
+  rename(sim = x)
+
+# 6) pick a sample to plot
+id = as.integer(runif(15, 1, length(unique(dat$sample_id))))
+
+# 7) Make Plots
+# violin
+post_pred_si = sim_posts %>%
+  # filter(sample_id %in% id) %>%
+  ggplot(aes(x = as.integer(.draw), y = sim, color = data, group = .draw)) + 
+  geom_violin() +
+  scale_y_log10() +
+  scale_x_continuous(breaks = c(0,1, 2,3, 4,5, 6,7, 8,9, 10),
+                     labels = c(expression(italic("y")), 
+                                "1", "2","3", "4","5", "6","7", "8","9", "10")) +
+  facet_wrap(~ site_id, ncol = 4) +
+  scale_color_colorblind(labels = expression(italic("y"[rep]) ~ "rep")) +
+  labs(y = "Individual Body Size (mgDM)",
+       x = "Draw",
+       color = "") +
+  guides(color = "none") +
+  theme(legend.position = c(0.8, 0.08)) +
+  NULL
+
+ggview::ggview(post_pred_si, width = 6.5, height = 9)
+ggsave(post_pred_si, file = "plots/post_pred_i.jpg", width = 6.5, height = 9)
+
+sim_posts %>%
+  filter(site_id == "ARIK") %>% 
+  # filter(sample_id %in% id) %>%
+  ggplot(aes(x = .draw, y = sim, color = data, group = .draw)) + 
+  geom_violin() +
+  scale_y_log10() +
+  facet_wrap(~ sample_id) +
+  NULL
+
+# density
+sim_posts %>%
+  ggplot(aes(x = sim, color = data, group = .draw)) + 
+  geom_density() +
+  scale_x_log10() +
+  facet_wrap(~site_id, scales = "free_y") +
+  scale_color_colorblind() +
+  NULL
+
+# r2 ----------------------------------------------------------------------
+# from: Gelman, A., Goodrich, B., Gabry, J., & Vehtari, A. (2019). R-squared for Bayesian regression models. The American Statistician.
+dat_resampled = dat %>% 
+  group_by(sample_id, log_gpp_s, log_om_s, mat_s, site_id, year) %>% 
+  sample_n(n_samples, weight = no_m2, replace = T) %>% 
+  select(site_id, year, sample_id, no_m2, dw) %>% 
+  group_by(sample_id) %>% 
+  mutate(xmin = min(dw),
+         xmax = max(dw),
+         data = "y_raw") %>% 
+  ungroup()
+
+
+set.seed = 23234
+
+preds = dat_resampled %>% 
+  slice_sample(n = 5000) %>% 
+  mutate(no_m2 = 1) %>% 
+  add_predicted_draws(fit_temp_om_gpp, re_formula = NA, ndraws = 1000) 
+
+preds %>% 
+  group_by(.draw) %>% 
+  reframe(v_fit = var(.prediction),
+          v_pred = var(dw - .prediction)) %>% 
+  mutate(r2 = v_fit/(v_fit + v_pred)) %>% 
+  reframe(mean = mean(r2),
+          sd = sd(r2))
+
+
+# confirm r2 - the bayes_R2() function in brms does not work with isd models. The code below checks that our calculation of R2 above is correct. It does this by
+# comparing the hand-calculated R2 from a basic stan model to the bayes_R2(). There is an exact match, suggesting that our calculation above is correct.
+
+# mod_test = brm(mpg ~ hp, data = mtcars,
+#                family = "Gaussian")
+
+as_tibble(t(fitted(mod_test, summary = F))) %>%
+  mutate(y = mod_test$data$mpg) %>%
+  pivot_longer(cols = -y) %>%
+  mutate(.draw = parse_number(name)) %>%
+  group_by(.draw) %>%
+  reframe(v_fit = var(value),
+          v_pred = var(y - value)) %>%
+  mutate(r2 = v_fit/(v_fit + v_pred)) %>%
+  mean_qi(r2)
+
+bayes_R2(mod_test)
---
+++ code/3.1) check_models_geometric_mean_bayes_p.R
@@ -0,0 +1,168 @@
+library(rstan)
+library(tidyverse)
+library(janitor)
+library(tidybayes)
+library(brms)
+library(ggthemes)
+library(isdbayes)
+library(ggview)
+theme_set(brms::theme_default())
+
+# load these
+fitdat2 = readRDS(file = "data/fitdat2.rds") # raw re-sampled data
+fit_temp_om_gpp = readRDS("models/fit_temp_om_gpp.rds") # bayesian model
+post_preds2 = readRDS(file = "posteriors/post_preds2.rds") # extracted posteriors of 133 samples (load to avoid re-running)
+
+# 1) Wrangle  -------------------------------------------------------------
+
+# resample raw data
+n_pred = 5000 # number of individuals to sample. This is 5000 for the ms. But it works ok with 500 as well.
+
+fitdat_temp = fit_temp_om_gpp$data %>% 
+  # filter(sample_id %in% 1:50) %>% 
+  group_by(sample_id) %>% 
+  sample_n(size = n_pred, replace = T, weight = no_m2) %>% 
+  arrange(-dw) %>% 
+  mutate(n_yx = 1:max(row_number()),
+         n = max(row_number()),
+         ismax = dw - xmax) 
+
+# get max and min to add
+# get_maxmin = fitdat_temp %>% 
+#   select(-dw, -no_m2, -n_yx) %>% 
+#   distinct() %>% 
+#   mutate(dwxmin = xmin,
+#          dwxmax = xmax) %>% 
+#   pivot_longer(cols = starts_with("dw"),
+#                values_to = "dw") %>% 
+#   select(-name)
+
+# # get sequential sample ids within site (for plotting)
+sequential_sample_ids = fitdat_temp %>%
+  ungroup %>%
+  distinct(site_id, sample_id) %>%
+  arrange(site_id, sample_id) %>%
+  group_by(site_id) %>%
+  mutate(sample_id_seq = row_number())
+
+# add max and min
+fitdat2 = bind_rows(fitdat_temp) %>% 
+  left_join(sequential_sample_ids)
+
+saveRDS(fitdat2, file = "data/fitdat2.rds")
+
+# 2) Sample Posterior  -------------------------------------------------------------
+
+# sample yrep from posterior
+sample_posts = fit_temp_om_gpp$data %>% 
+  select(-dw, -no_m2) %>% 
+  distinct() %>% 
+  mutate(no_m2 = 1) %>% 
+  add_epred_draws(fit_temp_om_gpp, re_formula = NULL, ndraws = 300) %>% 
+  ungroup %>% 
+  distinct(sample_id, .draw, .epred)
+
+post_preds2 = sample_posts %>% filter(.draw <= 100) %>% 
+  left_join(sequential_sample_ids) %>% 
+  left_join(fitdat2 %>% ungroup %>% distinct(sample_id, xmin, xmax)) %>%
+  expand_grid(individual = 1:n_pred) %>% 
+  ungroup  %>% 
+  mutate(yrep = rparetocounts(nrow(.), lambda = .epred, xmin = xmin, xmax = xmax)) 
+
+saveRDS(post_preds2, file = "posteriors/post_preds2.rds")
+
+# 3) Plot Summary Statistics  -------------------------------------------------------------
+
+# calculate summary statistic
+sample_gm = fitdat2 %>% 
+  group_by(sample_id,sample_id_seq, site_id) %>% 
+  reframe(gm = exp(mean(log(dw))),
+          median_dw = median(dw)) 
+
+# plot posts vs raw summary statistics
+# randomly select 1 sample from each site
+sample_ids = fitdat2 %>% ungroup %>% distinct(sample_id, site_id) %>% 
+  group_by(site_id) %>% 
+  sample_n(1) %>% 
+  pull(sample_id)
+
+reorder_ids = post_preds2 %>% 
+  ungroup %>% 
+  group_by(sample_id_seq, site_id) %>% 
+  reframe(rank_gm = exp(mean(log(yrep)))) %>% 
+  arrange(rank_gm) %>% 
+  mutate(sample_id_ordered = 1:nrow(.))
+
+post_pred_gm_summary = post_preds2 %>% 
+  # filter(sample_id_seq == 2) %>%
+  group_by(sample_id_seq, site_id, xmin, xmax, .draw) %>% 
+  reframe(gm = exp(mean(log(yrep))),
+          median_dw = median(yrep)) %>% 
+  left_join(reorder_ids)
+
+post_pred_gm = post_pred_gm_summary %>% 
+  ggplot(aes(x = sample_id_ordered, y = gm)) +
+  stat_pointinterval(alpha = 0.5,
+                     size = 0.2,
+                     aes(color = "Posterior Predictive", shape = "Posterior Predictive")) +
+  scale_y_log10() +
+  geom_point(data = sample_gm %>% 
+               left_join(reorder_ids), 
+             aes(y = gm, color = "Raw Data", shape = "Raw Data"),
+             size = 1) +
+  guides(fill = "none") +
+  scale_color_manual(values = c("#56b4e9", "black")) +
+  # scale_shape_manual(values = c(16, 17)) +
+  labs(x = "NEON Sample (n = 133)",
+       y = "Geometric mean body size (mgDM)") +
+  guides(shape = "none") +
+  guides(color = guide_legend(override.aes = list(shape = c(16, 17)),
+                              title = "")) +
+  theme(legend.position = c(0.2, 0.95))
+
+ggview(post_pred_gm, width = 6.5, height = 5.5)
+saveRDS(post_pred_gm, file = "plots/post_pred_gm.rds")
+ggsave(post_pred_gm, file = "plots/post_pred_gm.jpg", width = 6.5, height = 5.5)       
+       
+     
+post_pred_median = post_preds2 %>% 
+  # filter(sample_id_seq == 2) %>%
+  group_by(sample_id_seq, site_id, xmin, xmax, .draw) %>% 
+  reframe(gm = exp(mean(log(yrep))),
+          median_dw = median(yrep)) %>% 
+  left_join(reorder_ids) %>% 
+  ggplot(aes(x = sample_id_ordered, y = median_dw)) +
+  stat_pointinterval(alpha = 0.5, aes(color = "Posterior Predictive", shape = "Posterior Predictive")) +
+  scale_y_log10() +
+  geom_point(data = sample_gm %>% 
+               left_join(reorder_ids), 
+             aes(color = "Raw Data", shape = "Raw Data"),
+             size = 2.3) +
+  guides(fill = "none") +
+  scale_color_manual(values = c("#56b4e9", "black")) +
+  scale_shape_manual(values = c(16, 17)) +
+  labs(x = "NEON Sample (n = 133)",
+       y = "Geometric mean body size (mgDM)") 
+
+# 4) Bayesian P-Values  -------------------------------------------------------------
+
+bayes_p = post_preds2 %>% 
+  # filter(sample_id_seq == 2) %>%
+  group_by(sample_id_seq, site_id, xmin, xmax, .draw) %>% 
+  reframe(gm = exp(mean(log(yrep)))) %>% 
+  left_join(reorder_ids) %>% 
+  left_join(sample_gm %>% 
+              left_join(reorder_ids) %>% 
+              rename(gm_raw = gm)) %>% 
+  mutate(diff = gm - gm_raw,
+         maxdraws = max(.draw)) %>% 
+  group_by(sample_id, maxdraws) %>% 
+  reframe(sumdiff = sum(diff>0)) %>% 
+  mutate(bayes_p = sumdiff/maxdraws)
+
+range(bayes_p$bayes_p)
+mean(bayes_p$bayes_p)
+sd(bayes_p$bayes_p)
+bayes_p %>% 
+  filter(bayes_p > 0.9 | bayes_p <0.1)
+
