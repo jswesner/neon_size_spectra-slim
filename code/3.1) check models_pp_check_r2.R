815259d8790c97ed0763809f3c1077839e5fdf3c
Jeff Wesner <jeff.wesner@usd.edu>
2026-06-01 14:11:53 -0500

    update pp_check

--- code/3.1) check models_pp_check_r2.R
+++ code/3.1) check models_pp_check_r2.R
@@ -1,3 +1,4 @@
+# conduct posterior predictive checks and calculate Bayesian R2
 library(rstan)
 library(tidyverse)
 library(janitor)
@@ -28,93 +29,6 @@ pp_check_plot = (pp_check_global + theme(legend.position = c(0.8, 0.9)) +
 
 ggsave(pp_check_plot, file = "plots/fig_s6_pp_check_plot.jpg", width = 6.5, height = 9, dpi = 400)
 
-# 
-# # 3) extract posteriors 
-# posts_sample_lambdas = dat %>% 
-#   distinct(sample_id, .keep_all = T) %>% 
-#   select(-dw) %>% 
-#   mutate(no_m2 = 1) %>% 
-#   add_epred_draws(mod, re_formula = NULL) %>% 
-#   rename(lambda = .epred) %>% 
-#   ungroup()
-# 
-# saveRDS(posts_sample_lambdas, file = "posteriors/posts_sample_lambdas.rds")
-# 
-# # 4) merge posts and raw data
-# n_samples = 5000
-# 
-# dat_resampled = dat %>% 
-#   group_by(sample_id, log_om_s, log_gpp_s, mat_s, site_id, year) %>% 
-#   sample_n(n_samples, weight = no_m2, replace = T) %>% 
-#   select(site_id, year, sample_id, no_m2, dw) %>% 
-#   group_by(sample_id) %>% 
-#   mutate(xmin = min(dw),
-#          xmax = max(dw),
-#          data = "y_raw") %>% 
-#   ungroup()
-# 
-# posts_raw = posts_sample_lambdas %>% 
-#   filter(.draw <= 10) %>% 
-#   select(lambda, sample_id, .draw) %>% 
-#   right_join(dat_resampled %>% 
-#                group_by(sample_id) %>% 
-#                slice_sample(n = 500) %>% 
-#                select(sample_id, dw, no_m2, xmin, xmax, site_id), multiple = "all",
-#              relationship = "many-to-many") 
-# 
-# # 5) sample posterior preds
-# sim_posts = posts_raw %>% 
-#   mutate(x = rparetocounts(lambda = lambda, xmin = xmin, xmax = xmax, n = nrow(.))) %>% 
-#   mutate(data = "y_rep") %>% 
-#   # mutate(x = round(x,2)) %>% 
-#   bind_rows(dat_resampled %>% 
-#               mutate(data = "y") %>% 
-#               mutate(.draw = 0,
-#                      x = dw)) %>% 
-#   rename(sim = x)
-# 
-# # 6) pick a sample to plot
-# id = as.integer(runif(15, 1, length(unique(dat$sample_id))))
-# 
-# # 7) Make Plots
-# # violin
-# post_pred_si = sim_posts %>%
-#   # filter(sample_id %in% id) %>%
-#   ggplot(aes(x = as.integer(.draw), y = sim, color = data, group = .draw)) + 
-#   geom_violin() +
-#   scale_y_log10() +
-#   scale_x_continuous(breaks = c(0,1, 2,3, 4,5, 6,7, 8,9, 10),
-#                      labels = c(expression(italic("y")), 
-#                                 "1", "2","3", "4","5", "6","7", "8","9", "10")) +
-#   facet_wrap(~ site_id, ncol = 4) +
-#   scale_color_colorblind(labels = expression(italic("y"[rep]) ~ "rep")) +
-#   labs(y = "Individual Body Size (mgDM)",
-#        x = "Draw",
-#        color = "") +
-#   guides(color = "none") +
-#   theme(legend.position = c(0.8, 0.08)) +
-#   NULL
-# 
-# # ggview::ggview(post_pred_si, width = 6.5, height = 9)
-# ggsave(post_pred_si, file = "plots/post_pred_i.jpg", width = 6.5, height = 9)
-# 
-# sim_posts %>%
-#   filter(site_id == "ARIK") %>% 
-#   # filter(sample_id %in% id) %>%
-#   ggplot(aes(x = .draw, y = sim, color = data, group = .draw)) + 
-#   geom_violin() +
-#   scale_y_log10() +
-#   facet_wrap(~ sample_id) +
-#   NULL
-# 
-# # density
-# sim_posts %>%
-#   ggplot(aes(x = sim, color = data, group = .draw)) + 
-#   geom_density() +
-#   scale_x_log10() +
-#   facet_wrap(~site_id, scales = "free_y") +
-#   scale_color_colorblind() +
-#   NULL
 
 # r2 ----------------------------------------------------------------------
 # from: Gelman, A., Goodrich, B., Gabry, J., & Vehtari, A. (2019). R-squared for Bayesian regression models. The American Statistician.
