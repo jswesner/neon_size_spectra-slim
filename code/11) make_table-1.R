70a3acf7a2c7ee8e89d641d8e6f255cab74b8f83
Jeff Wesner <jeff.wesner@usd.edu>
2026-06-01 15:04:10 -0500

    update predictors to have mean and sd

--- code/11) make_table-1.R
+++ code/11) make_table-1.R
@@ -6,11 +6,16 @@ library(isdbayes)
 # Literature Figure Comparisons -------------------------------------------
 theme_set(theme_default())
 
-dat_all = readRDS("data/derived_data/dat_all.rds")
+predictors = readRDS("data/predictors.rds")
 
-dat_all %>% 
-  distinct(temp_mean, temp_sd,
-           gpp, gpp_sd,
-           mean_om, sd_om) %>% 
+predictors %>% 
+  distinct(site_id,
+           mean_temp,
+           sd_temp,
+           mean_gpp,
+           sd_gpp,
+           mean_om,
+           sd_om
+           ) %>% 
   mutate(across(where(is.numeric), round, 0))%>% 
   write_csv(., file = "tables/environmental_summary.csv")
